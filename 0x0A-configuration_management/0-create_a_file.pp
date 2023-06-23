fe5ae6d17c6f3f3d972ca6afa444744208d9efe5
Feke_21 <favouredubio@gmail.com>
2023-06-23 18:33:19 +0000

    Trying to fix accord. to puppet lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Creating a file in /tmp
 
-file { '/tmp/school':	
-  ensure => file,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
